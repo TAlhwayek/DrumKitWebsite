29f9c8437f6551bb3e7c67442662be71f723986c
TAlhwayek <60246873+TAlhwayek@users.noreply.github.com>
2021-12-26 22:34:43 -0800

    Added keyboard support
    Buttons now take in keyboard input. Alert in the switch statement (default) was removed and replaced with a simple console log.

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-
-
-
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-// Waiting for a click
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+  // Waiting for a click
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     // Assigning a value to the clicked button
     var buttonInnerHTML = this.innerHTML;
 
-      // Switch statement for each individual letter
-      switch (buttonInnerHTML) {
-        case "w":
-          var crash = new Audio("sounds/crash.mp3");
-          crash.play();
-        break;
+    // Waiting for clicks to play sounds
+    makeSound(buttonInnerHTML);
+  });
+}
 
-        case "a":
-          var kick = new Audio("sounds/kick-bass.mp3");
-          kick.play();
-        break;
 
-        case "s":
-          var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-        break;
+// Listening for keystrokes to play sounds
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+})
 
-        case "d":
-          var tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-        break;
 
-        case "j":
-          var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-        break;
+// Function that uses each letter to play a sound
+function makeSound(key) {
 
-        case "k":
-          var tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-        break;
+  // Switch statement for each individual letter
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
 
-        case "l":
-          var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-        break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
 
-        default: alert(buttonInnerHTML);
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
 
-      }
-      });
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+
+    // Default statement just to have something in here
+    default:
+      console.log(key + " is not a supported button.");
+
+  }
 }
