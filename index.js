db6a23c7be749caa56e82f383310a78e1b58fadb
TAlhwayek <60246873+TAlhwayek@users.noreply.github.com>
2021-12-27 00:24:08 -0800

    Added a score text
    Text that cheers you on is the highlight and tweaked some other things.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Score
+var score = 0;
+
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
   // Waiting for a click
@@ -8,54 +11,63 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
     // Waiting for clicks to play sounds
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
+
 
 
 // Listening for keystrokes to play sounds
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
-
 // Function that uses each letter to play a sound
+// Switch statement for each individual letter
 function makeSound(key) {
 
-  // Switch statement for each individual letter
   switch (key) {
     case "w":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
+      score++;
       break;
 
     case "a":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
+      score++;
       break;
 
     case "s":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
+      score++;
       break;
 
     case "d":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
+      score++;
       break;
 
     case "j":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
+      score++;
       break;
 
     case "k":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
+      score++;
       break;
 
     case "l":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
+      score++;
       break;
 
 
@@ -64,4 +76,21 @@ function makeSound(key) {
       console.log(key + " is not a supported button.");
 
   }
+  checkScore(score);
+}
+
+function checkScore(score) {
+if (score > 50) {
+  document.getElementById("score").style.fontSize = "x-large";
+  document.getElementById("score").innerHTML = "KEEP ON ROCKING!";
+  document.getElementById("score").style.color = "red";
+}
+}
+// Adding animations for button presses
+function buttonAnimation(currentKey) {
+  var pressedButton = document.querySelector("." + currentKey)
+  pressedButton.classList.add("pressed");
+  setTimeout(function() {
+  pressedButton.classList.remove("pressed");
+}, 80);
 }
